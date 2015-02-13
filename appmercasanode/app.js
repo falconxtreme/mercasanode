247abbdfc28cbd46885659ad060d0a7022613151
falconxtreme <falcon.donovan@gmail.com>
2015-02-13 03:07:06 -0500

    modificacioens

--- appmercasanode/app.js
+++ appmercasanode/app.js
@@ -6,9 +6,21 @@
 var express = require('express')
   , routes = require('./routes');
 
+var routesIndex = require('./routes/index');
+
 var app = module.exports = express.createServer();
+var DATABASE = require('./custom_modules/database');
+//interaction DATABASE
+var BD = new DATABASE();
+//BD.createDataBase();
+//BD.createTables();
+/*BD.getAllProducts(function(res){
+   console.log(res);
+});*/
+
 
 // Configuration
+var port = process.env.PORT || 3000;
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
@@ -27,10 +39,27 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
+
 // Routes
+//app.get('/', routes.index);
+app.get('/', function (req, res) {
+    console.log("ingresa");
+    //var DATABASE = require('../custom_modules/database');
+    //var BD = new DATABASE();
+    var productosBDSave;
+    BD.getAllProducts(function (productosBD) {
+        productosBDSave = productosBD;
+        console.log("ingresa a bd");
+        res.render('index', { title: 'MERCASA', productos: {} });        
+    });
+    console.log("sale de bd");
+    console.log(productosBDSave);
+    //res.render('index', { title: 'MERCASA', productos: productosBDSave });
+});
 
-app.get('/', routes.index);
+//var appExpress = express();
+//express.use('/', routesIndex);
 
-app.listen(3000, function(){
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
--- appmercasanode/routes/index.js
+++ appmercasanode/routes/index.js
@@ -1,8 +1,23 @@
 
 /*
  * GET home page.
- */
+ 
+console.log("ingresa index.js");
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+
+*/
+exports.index = function (req, res) {
+    console.log("ingresa");
+    //res.render('index', { title: 'Express' })
+    var DATABASE = require('../custom_modules/database');
+    var BD = new DATABASE();
+    var productosBDSave;
+    BD.getAllProducts(function (productosBD) {
+        productosBDSave = productosBD;
+        //console.log(productosBD);
+        console.log("ingresa a bd");        
+    });
+    console.log("sale de bd");
+    console.log(productosBDSave);
+    //res.render('index', { title: 'MERCASA', productos: productosBDSave });
 };
